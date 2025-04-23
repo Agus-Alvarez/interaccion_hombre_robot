cdfbf907534430812e6cf3ff14ef6ab00dd175c4
Agus-Alvarez <118945212+Agus-Alvarez@users.noreply.github.com>
2025-04-23 17:24:13 +0200

    Add files via upload

---
+++ falta_o_no.crg/falta_o_no.crg.pml
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="falta_o_no.crg" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="entrada limpia" src="html/entrada limpia.jpg" />
+        <File name="index" src="html/index.html" />
+        <File name="mano" src="html/mano.jpeg" />
+        <File name="patada" src="html/patada.jpg" />
+        <File name="patadados" src="html/patadados.jpeg" />
+        <File name="falta_o_no_service" src="src/falta_o_no_service.py" />
+    </Resources>
+    <Topics>
+        <Topic name="falta_o_no_topico" src="falta_o_no_topico.top" topicName="" language="" />
+    </Topics>
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ falta_o_no.crg/src/falta_o_no_service.py
@@ -0,0 +1,82 @@
+from naoqi import ALModule, ALProxy
+
+class FaltaONoService(ALModule):
+    def __init__(self, name):
+        ALModule.__init__(self, name)
+        self.tabletService = ALProxy("ALTabletService")
+        self.dialog = ALProxy("ALDialog")
+        self.imagenes = []
+        self.indice_imagen_actual = 0
+        self.nombre_topico = "falta_o_no_topico"
+
+    def iniciar_juego(self):
+        self.indice_imagen_actual = 0
+        self.cargar_imagenes()  # Método para cargar la lista de imágenes
+        if self.imagenes:
+            self.tabletService.loadApplication("falta_o_no")  # Carga la aplicación web
+            self.tabletService.showWebview("http://127.0.0.1/apps/falta_o_no/html/index.html") # Muestra index.html
+            self.mostrar_imagen()
+            self.dialog.activateTopic(self.nombre_topico)
+            self.dialog.subscribe(self.getName())
+        else:
+            self.dialog.say("No hay imágenes disponibles para el juego.")
+
+    def cargar_imagenes(self):
+        # Lista de imágenes con su información:
+        # "archivo": nombre del archivo de imagen en la carpeta 'html'
+        # "es_falta": True si es falta, False si no lo es
+        # "regla": Explicación o regla asociada a la jugada
+        self.imagenes = [
+            {"archivo": "entrada limpia.jpg", "es_falta": False, "regla": "Una entrada limpia al balón para ganar posesión no es falta."},
+            {"archivo": "mano.jpeg", "es_falta": True, "regla": "Tocar el balón deliberadamente con la mano es falta."},
+            {"archivo": "patada.jpg", "es_falta": True, "regla": "Dar una patada a un oponente es falta."},
+            {"archivo": "patadados.jpeg", "es_falta": True, "regla": "Dar una patada a un oponente es falta."},
+        ]
+
+    def mostrar_imagen(self):
+        if 0 <= self.indice_imagen_actual < len(self.imagenes):
+            ruta_imagen = "http://127.0.0.1/apps/falta_o_no/html/" + self.imagenes[self.indice_imagen_actual]["archivo"]
+            #  Llamamos a la función JavaScript para cambiar la imagen
+            self.tabletService.executeJS("cambiarImagen('" + ruta_imagen + "');")
+            self.dialog.say("Mira la imagen en la tableta. ¿Es falta o no?")
+        else:
+            self.dialog.say("¡Hemos visto todas las jugadas!")
+            self.finalizar_juego()
+
+    def verificar_respuesta(self, respuesta):
+        if not self.imagenes:
+            self.dialog.say("El juego no ha comenzado o no hay imágenes.")
+            return
+
+        respuesta_usuario = respuesta.lower()
+        es_falta_correcta = self.imagenes[self.indice_imagen_actual]["es_falta"]
+        regla = self.imagenes[self.indice_imagen_actual]["regla"]
+        self.indice_imagen_actual += 1
+
+        if (respuesta_usuario == "falta" and es_falta_correcta) or (respuesta_usuario == "no falta" and not es_falta_correcta):
+            self.dialog.say("¡Correcto! " + regla)
+            self.mostrar_imagen()
+        else:
+            self.dialog.say("Incorrecto. " + regla)
+            self.mostrar_imagen()
+
+    def finalizar_juego(self):
+        self.dialog.unsubscribe(self.getName())
+        self.dialog.deactivateTopic(self.nombre_topico)
+
+    #  Esta función será llamada desde JavaScript en la página web
+    def pepperRespuesta(self, respuesta):
+        self.verificar_respuesta(respuesta)
+
+    def main(session):
+        global module
+        module = FaltaONoService("FaltaONoService")
+        #  Necesitamos crear una instancia del servicio para poder usar pepperRespuesta
+        global falta_o_no_service
+        falta_o_no_service = module
+        session.registerService("FaltaONoService", module)
+
+    if __name__ == "__main__":
+        from naoqi import ALBroker
+        myBroker = ALBroker("myBroker", "0.0.0.0", 0, "127.0.0.1", 9559)
+        main(myBroker.getSession())
